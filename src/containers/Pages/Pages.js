08b2ec4afd7684e8d8e10504b8b730a0c6fcc32b
Ernur Kurmanbekov <erazavr85@gmail.com>
2019-12-27 17:11:32 +0600

    Created container Pages

---
+++ src/containers/Pages/Pages.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import axiosPages from "../../axiosApi";
+
+class Pages extends Component {
+    state = {
+        page: null,
+    };
+    requestData = async ()=> {
+        const response = await axiosPages.get(`/pages/${this.props.match.params.name}.json`);
+        if (response.data) {
+            this.setState({page: response.data});
+        }
+    };
+    componentDidMount() {
+        return this.requestData()
+    }
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.name !== this.props.match.params.name) {
+            return this.requestData()
+        }
+    }
+    render() {
+        return this.state.page &&(
+            <div style={{textAlign: 'center'}}>
+                <h1>{this.state.page.title}</h1>
+                <p style={{padding: '0 50px'}}>{this.state.page.content}</p>
+            </div>
+        );
+    }
+}
+
+export default Pages;
